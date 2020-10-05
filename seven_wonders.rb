60e1128029e8242f63c87cfe21e12881293ea7c8
Emily Nagai <emnagai89@gmail.com>
2020-10-05 16:00:21 -0700

    get_location(search_term) method implemented

--- seven_wonders.rb
+++ seven_wonders.rb
@@ -1,21 +1,34 @@
 require 'httparty'
 require "awesome_print"
 
-BASE_URL = "THE BASE URL FOR THE API REQUEST"
-LOCATION_IQ_KEY = "YOUR API TOKEN"
+BASE_URL = "https://us1.locationiq.com/v1/search.php"
+LOCATION_IQ_KEY = "pk.cc3ba91ebd591e2f240d75e3dd02dae1"
 
 def get_location(search_term)
+  response = HTTParty.get(BASE_URL, query: {
+      q: search_term,
+      key: LOCATION_IQ_KEY,
+      format: 'json'
+  }
+  )
+  query_parameter = {
+      search_term => {
+      "lat" => response[0]["lat"],
+      "lon" => response[0]["lon"]
+      }
+  }
 
 end
 
 def find_seven_wonders
-
   seven_wonders = ["Great Pyramid of Giza", "Gardens of Babylon", "Colossus of Rhodes", "Pharos of Alexandria", "Statue of Zeus at Olympia", "Temple of Artemis", "Mausoleum at Halicarnassus"]
 
   seven_wonders_locations = []
 
   seven_wonders.each do |wonder|
+    seven_wonders_hash = {}
     sleep(0.5)
+
     seven_wonders_locations << get_location(wonder)
   end
 
@@ -27,3 +40,4 @@ end
 ap find_seven_wonders
 # Expecting something like:
 # [{"Great Pyramid of Giza"=>{:lat=>"29.9791264", :lon=>"31.1342383751015"}}, {"Gardens of Babylon"=>{:lat=>"50.8241215", :lon=>"-0.1506162"}}, {"Colossus of Rhodes"=>{:lat=>"36.3397076", :lon=>"28.2003164"}}, {"Pharos of Alexandria"=>{:lat=>"30.94795585", :lon=>"29.5235626430011"}}, {"Statue of Zeus at Olympia"=>{:lat=>"37.6379088", :lon=>"21.6300063"}}, {"Temple of Artemis"=>{:lat=>"32.2818952", :lon=>"35.8908989553238"}}, {"Mausoleum at Halicarnassus"=>{:lat=>"37.03788265", :lon=>"27.4241455276707"}}]
+
